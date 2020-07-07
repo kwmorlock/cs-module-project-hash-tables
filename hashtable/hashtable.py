162f4ee8893b87e7404c3cdfa6e3735acdd1b2eb
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-07-07 01:38:05 -0400

    djb2

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -49,6 +49,7 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        return self.length/self.get_num_slots()
 
 
     def fnv1(self, key):
@@ -68,6 +69,11 @@ class HashTable:
         Implement this, and/or FNV-1.
         """
         # Your code here
+        hash=5381
+        for x in key:
+            hash = ((hash<<5)+hash)+ord(x)
+        return hash & 0xFFFFFFFF
+        
 
 
     def hash_index(self, key):
