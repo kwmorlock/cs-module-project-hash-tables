112cad6ba4e5e3f6d978fbeaad761d48704c3a3e
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-07-09 01:10:50 -0400

    expensive setup

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,11 +1,26 @@
 # Your code here
+#import math and or random maybe?
+# import math
+# import random
 
-cache = {}
+# cache = {}
 
 
 def expensive_seq(x, y, z):
     # Your code here
-    if x in cache:
+    cache = {}
+    n = (x, y, z)
+    if n in cache:
+        return cache[n]
+    elif x <= 0:
+        return y + z
+    else:
+        cache[n] = (
+            expensive_seq(x-1, y+1, z)
+            + expensive_seq(x-2, y+2, z*2)
+            + expensive_seq(x-3, y+3, z*3)
+        )
+    return cache[n]
 
 
 
